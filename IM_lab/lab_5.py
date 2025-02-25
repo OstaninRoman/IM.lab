f2cd19a99ef4213302a6067edd79e5221ed5f6d2
Roman <ostanin.roma15@gmail.com>
2025-02-25 21:09:25 +0700

    fix lab5

--- IM_lab/lab_5.py
+++ IM_lab/lab_5.py
@@ -1,85 +1,68 @@
 import math as m
 import numpy as np
 
-def SMO_Erlang(TM, Lambd, Mu, N):
-    Tm = TM
-    lambd = Lambd
-    mu = Mu
-    n = N
-    t = 0
-    T1 = 0
-    WT = 0
+def SMO_Erlang(Tm, lambd, mu, n):
     Queue = []
     MassDevice = [0]*n
+    T1 = 0
+    WT = 0
     EventCount = 0
-
     while T1 < Tm:
-        t = -(1/lambd)*m.log(1 - np.random.rand(1)[0])
-        T1 = T1 + t
-        t = 0
+        T1 = T1 + (-(1/lambd)*m.log(1 - np.random.rand(1)[0]))
         if T1 > Tm:
             flag = 0
-            for j in range(len(Queue)):
+            for i in range(len(Queue)):
                 if flag == 0:
                     MassDevice.sort()
                     if Queue[0] < MassDevice[0] < Tm:
                         WT = WT + (MassDevice[0] - Queue[0])
-                        t = -(1 / mu) * m.log(1 - np.random.rand(1)[0])
-                        MassDevice[0] = MassDevice[0] + t
-                        t = 0
+                        MassDevice[0] = MassDevice[0] + (-(1 / mu) * m.log(1 - np.random.rand(1)[0]))
                         Queue.pop(0)
                     else:
                         flag = 1
                 else:
                     break
-            if len(Queue) > 0:
-                for i in range(len(Queue)):
-                    WT = WT + (Tm - Queue[i])
+            EventCount = EventCount - len(Queue)
             break
-        ###
-        Queue.append(T1)
-        EventCount = EventCount + 1
-        flag = 0
-        for j in range(len(Queue)):
-            if flag == 0:
-                MassDevice.sort()
-                if len(Queue) == 1:
-                    if Queue[0] > MassDevice[0]:
-                        MassDevice[0] = Queue[0]
-                        t = -(1 / mu) * m.log(1 - np.random.rand(1)[0])
-                        MassDevice[0] = MassDevice[0] + t
-                        t = 0
-                        Queue.pop(0)
-                else:
-                    if Queue[0] < MassDevice[0] < Queue[len(Queue)-1]:
+        if len(Queue) > 0:
+            flag = 0
+            for i in range(len(Queue)):
+                if flag == 0:
+                    MassDevice.sort()
+                    if Queue[0] < MassDevice[0] < T1:
                         WT = WT + (MassDevice[0] - Queue[0])
-                        t = -(1 / mu) * m.log(1 - np.random.rand(1)[0])
-                        MassDevice[0] = MassDevice[0] + t
-                        t = 0
+                        MassDevice[0] = MassDevice[0] + (-(1 / mu) * m.log(1 - np.random.rand(1)[0]))
                         Queue.pop(0)
                     else:
                         flag = 1
+                        Queue.append(T1)
+                        EventCount = EventCount + 1
+                else:
+                    break
+        if len(Queue) == 0:
+            MassDevice.sort()
+            if T1 > MassDevice[0]:
+                MassDevice[0] = T1
+                MassDevice[0] = MassDevice[0] + (-(1 / mu) * m.log(1 - np.random.rand(1)[0]))
             else:
-                break
+                Queue.append(T1)
+                EventCount = EventCount + 1
     M_WT = (1/EventCount)*WT
     return M_WT
 
-Tm = int(input())
-lambd = float(input())
-mu = float(input())
+TM = int(input())
+Lambd = float(input())
+Mu = float(input())
 n = int(input())
 #2,1,3;
 
-Mass_Wt = []
-Wt = 0
+M_Wt = 0
 N = 1000
-Sum_Wt = 0
+Sum_MWt = 0
 
 for i in range(N+1):
-    Wt = SMO_Erlang(Tm, lambd, mu, n)
-    Mass_Wt.append(Wt)
-    Sum_Wt = Sum_Wt + Wt
+    M_Wt = SMO_Erlang(TM, Lambd, Mu, n)
+    Sum_MWt = Sum_MWt + M_Wt
 
-EstemetM_Wt = (1/N)*Sum_Wt
-print(Mass_Wt)
+EstemetM_Wt = (1/N)*Sum_MWt
 print("Оценка среднего времени простоя заявки в очереди: ", EstemetM_Wt)
